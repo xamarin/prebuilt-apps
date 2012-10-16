0a9dbd24d4de8170315ea4885168863f8a3eaa7e
Nick Peppers <nick.peppers@hitcents.com>
2012-10-16 17:11:10 -0500

    Added to the Database

--- FieldService/FieldService/Data/Database.cs
+++ FieldService/FieldService/Data/Database.cs
@@ -152,6 +152,108 @@ namespace FieldService.Data
                             ContactName = "HERB",
 			},
 
+                        new Assignment
+			{
+			    ID = 5,
+			    Priority = 5,
+                            JobNumber = "5",
+			    Title = "Assignment 5",
+			    Status = AssignmentStatus.New,
+			    StartDate = DateTime.Now.AddDays(1),
+			    EndDate = DateTime.Now.AddDays(1).AddHours(2),
+                            City = "Mountain View",
+                            Zip = "94043",
+                            State = "CA",
+                            Address = "1600 Amphitheatre Parkway",
+                            ContactPhone = "650.253.0000",
+                            ContactName = "Google",
+			},
+
+                        new Assignment
+			{
+			    ID = 6,
+			    Priority = 6,
+                            JobNumber = "6",
+			    Title = "Assignment 6",
+			    Status = AssignmentStatus.New,
+			    StartDate = DateTime.Now.AddDays(1),
+			    EndDate = DateTime.Now.AddDays(1).AddHours(2),
+                            City = "Hershey",
+                            Zip = "17033",
+                            State = "PA",
+                            Address = "200 Crystal A Drive",
+                            ContactPhone = "717.298.1298",
+                            ContactName = "Hershey",
+			},
+
+                        new Assignment
+			{
+			    ID = 7,
+			    Priority = 7,
+                            JobNumber = "7",
+			    Title = "Assignment 7",
+			    Status = AssignmentStatus.New,
+			    StartDate = DateTime.Now.AddDays(1),
+			    EndDate = DateTime.Now.AddDays(1).AddHours(2),
+                            City = "Redmond",
+                            Zip = "98052",
+                            State = "WA",
+                            Address = "1 157th Avenue Norhteast",
+                            ContactPhone = "425.882.8080",
+                            ContactName = "Microsoft",
+			},
+
+                        new Assignment
+			{
+			    ID = 8,
+			    Priority = 8,
+                            JobNumber = "8",
+			    Title = "Assignment 8",
+			    Status = AssignmentStatus.New,
+			    StartDate = DateTime.Now.AddDays(1),
+			    EndDate = DateTime.Now.AddDays(1).AddHours(2),
+                            City = "New York",
+                            Zip = "10118",
+                            State = "NY",
+                            Address = "350 5th Avenue",
+                            ContactPhone = "212.736.3100",
+                            ContactName = "The Empire",
+			},
+
+                        new Assignment
+                        {
+			    ID = 9,
+			    Priority = 9,
+                            JobNumber = "9",
+			    Title = "Assignment 9",
+			    Status = AssignmentStatus.New,
+			    StartDate = DateTime.Now.AddDays(1),
+			    EndDate = DateTime.Now.AddDays(1).AddHours(2),
+                            City = "Chehalis",
+                            Zip = "98532",
+                            State = "WA",
+                            Address = "465 Northwest Chehalis Avenue",
+                            ContactPhone = "360.748.3720",
+                            ContactName = "The Shire",
+			},
+
+                        new Assignment
+                        {
+			    ID = 10,
+			    Priority = 10,
+                            JobNumber = "10",
+			    Title = "Assignment 10",
+			    Status = AssignmentStatus.New,
+			    StartDate = DateTime.Now.AddDays(1),
+			    EndDate = DateTime.Now.AddDays(1).AddHours(2),
+                            City = "Universal City",
+                            Zip = "91608",
+                            State = "CA",
+                            Address = "100 Universal City Plaza",
+                            ContactPhone = "800.864.8377",
+                            ContactName = "Universal Studios",
+			},
+
                         //Some items
                         new Item
                         {
@@ -198,12 +300,16 @@ namespace FieldService.Data
 			    Assignment = 3,
 			    Item = 1,
 			},
+                        new AssignmentItem
+			{
+			    Assignment = 5,
+			    Item = 1,
+			},
 
                         //Some labor for assignments
                         new Labor
                         {
                             Assignment = 1,
-                            ID = 1,
                             Description = "Sheet Metal Screw Sorting",
                             Hours = TimeSpan.FromHours(10),
                             Type = LaborType.HolidayTime,
@@ -211,7 +317,6 @@ namespace FieldService.Data
                         new Labor
                         {
                             Assignment = 1,
-                            ID = 2,
                             Description = "Pipe Fitting",
                             Hours = TimeSpan.FromHours(4),
                             Type = LaborType.Hourly,
@@ -219,7 +324,6 @@ namespace FieldService.Data
                         new Labor
                         {
                             Assignment = 2,
-                            ID = 3,
                             Description = "Attaching Sheet Metal To PVC Pipe",
                             Hours = TimeSpan.FromHours(8),
                             Type = LaborType.OverTime,
@@ -227,7 +331,6 @@ namespace FieldService.Data
                         new Labor
                         {
                             Assignment = 3,
-                            ID = 4,
                             Description = "Sheet Metal / Pipe Decoupling",
                             Hours = TimeSpan.FromHours(4),
                             Type = LaborType.Hourly,
@@ -237,7 +340,6 @@ namespace FieldService.Data
                         new Expense
                         {
                             Assignment = 1,
-                            ID = 1,
                             Description = "Filled up tank at Speedway",
                             Category = ExpenseCategory.Gas,
                             Cost = 40.5M,
@@ -245,7 +347,6 @@ namespace FieldService.Data
                         new Expense
                         {
                             Assignment = 1,
-                            ID = 2,
                             Description = "Tasty Hot Dog from Speedway",
                             Category = ExpenseCategory.Food,
                             Cost = 0.99M,
@@ -253,7 +354,6 @@ namespace FieldService.Data
                         new Expense
                         {
                             Assignment = 2,
-                            ID = 3,
                             Description = "Duct Tape",
                             Category = ExpenseCategory.Supplies,
                             Cost = 3.5M,
@@ -261,10 +361,51 @@ namespace FieldService.Data
                         new Expense
                         {
                             Assignment = 3,
-                            ID = 4,
                             Description = "Passable Hot Dog from Speedway",
                             Category = ExpenseCategory.Food,
                             Cost = 0.99M,
+                        },
+                        new Expense
+                        {
+                            Assignment = 5,
+                            Description = "Cost of Android being herp derp",
+                            Category = ExpenseCategory.Other,
+                            Cost = 255.5M,
+                        },
+                        new Expense
+                        {
+                            Assignment = 6,
+                            Description = "Chocolate",
+                            Category = ExpenseCategory.Food,
+                            Cost = 1.5M,
+                        },
+                        new Expense
+                        {
+                            Assignment = 7,
+                            Description = "Advertising",
+                            Category = ExpenseCategory.Other,
+                            Cost = 200M,
+                        },
+                        new Expense
+                        {
+                            Assignment = 8,
+                            Description = "Cleaning Supplies",
+                            Category = ExpenseCategory.Supplies,
+                            Cost = .006M,
+                        },
+                        new Expense
+                        {
+                            Assignment = 9,
+                            Description = "Ingredients",
+                            Category = ExpenseCategory.Food,
+                            Cost = .0001M,
+                        },
+                        new Expense
+                        {
+                            Assignment = 10,
+                            Description = "Universal Stuff",
+                            Category = ExpenseCategory.Supplies,
+                            Cost = 9.1M,
                         },
                     });
 
