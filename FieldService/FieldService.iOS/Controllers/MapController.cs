d1de2564c61802fb9bcaccb44218b0bbb3de8aa7
Jonathan Peppers <jonathan.peppers@hitcents.com>
2012-10-23 13:37:45 -0500

    Added driving directions for iOS

--- FieldService/FieldService.iOS/Controllers/MapController.cs
+++ FieldService/FieldService.iOS/Controllers/MapController.cs
@@ -13,9 +13,11 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 using System;
+using System.Text;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using MonoTouch.MapKit;
+using FieldService.Data;
 using FieldService.Utilities;
 
 namespace FieldService.iOS
@@ -41,6 +43,7 @@ namespace FieldService.iOS
 			mapView = new MKMapView(View.Frame);
 			mapView.ShowsUserLocation = true;
 			mapView.AutoresizingMask = View.AutoresizingMask;
+			mapView.Delegate = new MapViewDelegate();
 
 			View.BackgroundColor = UIColor.Clear;
 			View.AddSubview (mapView);
@@ -53,6 +56,82 @@ namespace FieldService.iOS
 			//Load the placemark
 			mapView.ClearPlacemarks ();
 			mapView.AddPlacemark (detailsController.Assignment.ToPlacemark());
+		}
+
+		/// <summary>
+		/// Delegate for the map view
+		/// </summary>
+		private class MapViewDelegate : MKMapViewDelegate
+		{
+			const string UserIdentifier = "UserAnnotation";
+			const string AssignmentIdentifier = "AssignmentAnnotation";
+			readonly UIPopoverController popoverController;
+
+			public MapViewDelegate ()
+			{
+				popoverController = new UIPopoverController(new UIViewController());
+				popoverController.PopoverContentSize = new System.Drawing.SizeF(100, 100);
+			}
+
+			/// <summary>
+			/// Returns our custom MKAnnotationView
+			/// </summary>
+			public override MKAnnotationView GetViewForAnnotation (MKMapView mapView, NSObject annotation)
+			{
+				var userLocation = annotation as MKUserLocation;
+				if (userLocation != null ) {
+					var annotationView = mapView.DequeueReusableAnnotation (UserIdentifier) as MKPinAnnotationView;
+					if (annotationView == null) {
+						annotationView = new MKPinAnnotationView(annotation, UserIdentifier);
+						annotationView.CanShowCallout = true;
+						annotationView.AnimatesDrop = false;
+					} else {
+						annotationView.Annotation = annotation;
+					}
+					return annotationView;
+				} else {
+					var annotationView = mapView.DequeueReusableAnnotation (AssignmentIdentifier) as MKPinAnnotationView;
+					if (annotationView == null) {
+						annotationView = new MKPinAnnotationView(annotation, AssignmentIdentifier);
+						annotationView.PinColor = MKPinAnnotationColor.Green;
+						annotationView.AnimatesDrop = true;
+						annotationView.CanShowCallout = true;
+						annotationView.RightCalloutAccessoryView = UIButton.FromType (UIButtonType.DetailDisclosure);
+					} else {
+						annotationView.Annotation = annotation;
+					}
+					return annotationView;
+				}
+			}
+
+			/// <summary>
+			/// This is the callback for when the detail disclosure is clicked
+			/// </summary>
+			public override void CalloutAccessoryControlTapped (MKMapView mapView, MKAnnotationView view, UIControl control)
+			{
+				var assignment = GetAssignment (view.Annotation as MKPlacemark);
+				StringBuilder builder = new StringBuilder("http://maps.apple.com/maps?daddr=");
+				builder.Append (assignment.Address.Replace (' ', '+'));
+				builder.Append ('+');
+				builder.Append (assignment.City.Replace (' ', '+'));
+				builder.Append ('+');
+				builder.Append (assignment.State);
+				builder.Append ('+');
+				builder.Append (assignment.Zip);
+				builder.Append ('+');
+
+				using (var url = NSUrl.FromString (builder.ToString ())) {
+					UIApplication.SharedApplication.OpenUrl (url);
+				}
+			}
+
+			/// <summary>
+			/// This pulls out an assignment we placed in a MKPlacemark
+			/// </summary>
+			private Assignment GetAssignment(MKPlacemark annotation)
+			{
+				return annotation.AddressDictionary[new NSString("Assignment")].UnwrapObject<Assignment>();
+			}
 		}
 	}
 }
