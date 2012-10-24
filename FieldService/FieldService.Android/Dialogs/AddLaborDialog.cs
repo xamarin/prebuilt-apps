8319b3c5adba92f6285dcefed9cc3f18cbcc61d5
Bryan Phillips <bryan.phillips@hitcents.com>
2012-10-24 15:02:06 -0500

    Adding labor hours popup

---
+++ FieldService/FieldService.Android/Dialogs/AddLaborDialog.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FieldService.Android.Dialogs {
+    class AddLaborDialog {
+    }
+}
