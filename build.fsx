62987915d015030a42cabd0dc02be046e8bf1668
David Siegel <djsiegel@gmail.com>
2013-12-20 15:54:00 -0800

    Try to build Android packages

--- build.fsx
+++ build.fsx
@@ -23,12 +23,20 @@ Target "Build" <| fun _ ->
     BuildiOSPackage "FieldService/FieldService.iOS/FieldService.iOS.csproj"
     BuildiOSPackage "EmployeeDirectory/EmployeeDirectory.iOS/EmployeeDirectory.iOS.csproj"
 
+    BuildAndroidPackage "FieldService/FieldService.Android/FieldService.Android.csproj"
+    BuildAndroidPackage "EmployeeDirectory/EmployeeDirectory.Android/EmployeeDirectory.Android.csproj"
+
 Target "Clean" <| fun _ ->
     CleanDirs [
         "FieldService/FieldService.iOS/bin"
         "FieldService/FieldService.iOS/obj"
         "EmployeeDirectory/EmployeeDirectory.iOS/bin"
         "EmployeeDirectory/EmployeeDirectory.iOS/obj"
+
+        "FieldService/FieldService.Android/bin"
+        "FieldService/FieldService.Android/obj"
+        "EmployeeDirectory/EmployeeDirectory.Android/bin"
+        "EmployeeDirectory/EmployeeDirectory.Android/obj"
     ]
 
 "Clean"
