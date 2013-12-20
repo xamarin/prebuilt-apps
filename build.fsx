0280d882a2b0ac98ddd998b083de9f594124f571
David Siegel <djsiegel@gmail.com>
2013-12-20 15:45:14 -0800

    Build EmployeeDirectory for iOS

--- build.fsx
+++ build.fsx
@@ -16,15 +16,19 @@ open Fake.XamarinHelper
 
 Target "Restore" <| fun _ ->
     RestoreComponents "FieldService/FieldService.Xamarin.sln"
+    RestoreComponents "EmployeeDirectory/EmployeeDirectory.Xamarin.sln"
     RestorePackages ()
 
 Target "Build" <| fun _ ->
     BuildiOSPackage "FieldService/FieldService.iOS/FieldService.iOS.csproj"
+    BuildiOSPackage "EmployeeDirectory/EmployeeDirectory.iOS/EmployeeDirectory.iOS.csproj"
 
 Target "Clean" <| fun _ ->
     CleanDirs [
         "FieldService/FieldService.iOS/bin"
         "FieldService/FieldService.iOS/obj"
+        "EmployeeDirectory/EmployeeDirectory.iOS/bin"
+        "EmployeeDirectory/EmployeeDirectory.iOS/obj"
     ]
 
 "Clean"
