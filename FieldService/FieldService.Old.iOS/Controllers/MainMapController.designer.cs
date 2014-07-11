04b788fd14757f06d4268343efbaf86275a51203
Luis Eduardo Hernandez Diaz <biz0505@hotmail.com>
2014-07-11 15:53:16 -0500

    Missing files
    Missing files

---
+++ FieldService/FieldService.Old.iOS/Controllers/MainMapController.designer.cs
@@ -0,0 +1,82 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.CodeDom.Compiler;
+
+namespace FieldService.iOS
+{
+	[Register ("MainMapController")]
+	partial class MainMapController
+	{
+		[Outlet]
+		MonoTouch.MapKit.MKMapView mapView { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIView activeAssignment { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton assignmentButton { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIImageView priorityBackground { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel priority { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel numberAndDate { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel titleLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel startAndEnd { get; set; }
+
+		[Outlet]
+		FieldService.iOS.TextButton contact { get; set; }
+
+		[Outlet]
+		FieldService.iOS.TextButton address { get; set; }
+
+		[Outlet]
+		FieldService.iOS.StatusButton status { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIImageView timerBackgroundImage { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UILabel timerLabel { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton record { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIImageView toolbarShadow { get; set; }
+
+		[Action ("ActiveAssignmentSelected")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void ActiveAssignmentSelected (MonoTouch.UIKit.UIButton sender);
+
+		[Action ("Address")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void Address (FieldService.iOS.TextButton sender);
+
+		[Action ("Record")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void Record (FieldService.iOS.StatusButton sender);
+
+		[Action ("Record")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void Record (MonoTouch.UIKit.UIButton sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
---
+++ FieldService/FieldService.Old.iOS/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace FieldService.iOS
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main (string[] args)
+		{
+			//Setup our services for core library
+			ServiceRegistrar.Startup ();
+
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}
+}
